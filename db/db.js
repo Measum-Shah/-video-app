51715c32816595b42aaad1a5fb2fc5730069321c
Measum-Shah <shahmeasum008@gmail.com>
2025-03-01 23:46:17 +0500

    I Made 3 utulities APIERROR, APIRESPONSE and ASYNCHANDLER

--- db/db.js
+++ db/db.js
@@ -9,7 +9,7 @@ configDotenv(
 const connectDB = async () => {
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-        console.log(`|n MOngoDB connect at DB Host : ${connectionInstance.connection.host}`)
+        console.log(`MOngoDB connect at DB Host : ${connectionInstance.connection.host}`)
    
     }
     catch(error){
