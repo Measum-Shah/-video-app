66b60c9d07fe4212bd2335dc7cc74452e5f385ea
Measum-Shah <shahmeasum008@gmail.com>
2025-03-01 22:30:32 +0500

    Setup the database connection

---
+++ db/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/constants.js";
+import { config as configDotenv } from "dotenv";
+
+configDotenv(
+    {path: '.env'}
+)
+
+const connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        console.log(`|n MOngoDB connect at DB Host : ${connectionInstance.connection.host}`)
+   
+    }
+    catch(error){
+        console.log("MONGODB connection error",error);
+        process.exit(1);
+
+    }
+}
+
+export default connectDB;
