199815fbf0416d96353b471fc56f464b06ce9117
Measum-Shah <shahmeasum008@gmail.com>
2025-03-03 22:53:50 +0500

    Add video model and implement user password hashing and JWT token generation

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,7 @@
  import mongoose, {Schema} from "mongoose";
+ import bcrypt from "bcrypt";
+ import jwt from "jsonwebtoken";
+
 
 const userSchema = new Schema({
 username : {
@@ -49,6 +52,42 @@ refreshToken:{
     timestamps:true
 })
 
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        return next()
+    }
+    else{
+    this.password = bcrypt.hashSync(this.password, 10); 
+    next();
+    }
+})
+
+userSchema.methods.isPasswordCorrrect = async function(password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return  jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    } )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return  jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    } )
+
+} 
 
 
 
